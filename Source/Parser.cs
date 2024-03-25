d518cec505e28644cd73749e3426fbbbe0201acf
Victor Romanov <gerter666@hotmail.com>
2024-03-25 17:08:06 +0300

    parser working with operations order(not with bracers yet and i don't it all)

--- Source/Parser.cs
+++ Source/Parser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace FormulasVisualizer.Source {
 
@@ -27,7 +28,12 @@ namespace FormulasVisualizer.Source {
      */
     public class FormulaParser {
 
-        private readonly IReadOnlyDictionary<char, OperatorPriority> _delimeters = new Dictionary<char, OperatorPriority>() {
+        private static IReadOnlyList<char> _numbers = new List<char>() {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '0'
+        };
+
+        private static IReadOnlyDictionary<char, OperatorPriority> _operators = new Dictionary<char, OperatorPriority>() {
+            ['='] = OperatorPriority.PRIORITY_FIRST,
             ['^'] = OperatorPriority.PRIORITY_FIRST,
             ['*'] = OperatorPriority.PRIORITY_SECOND,
             ['/'] = OperatorPriority.PRIORITY_SECOND,
@@ -35,30 +41,53 @@ namespace FormulasVisualizer.Source {
             ['-'] = OperatorPriority.PRIORITY_THIRD
         };
 
+        private int _replacesCount = 0;
+
         public Queue<Operation> queue { get; } = new Queue<Operation>();
-        private char replacer = 'a';
+        public List<string> _possibleVariables = new List<string>();
+
+        private bool IsNumber(string possibleNumber) {
+            return possibleNumber.All(x => _numbers.Contains(x));
+        }
+
+        private bool IsReplacer(string possibleReplacer) {
+            return possibleReplacer.Contains("EXPRESSION_RESULT_");
+        }
+
+        private string GenerateReplacer() {
+            _replacesCount++;
+            return $"EXPRESSION_RESULT_{_replacesCount}";
+        }
 
         public void Parse(string s, OperatorPriority priority) {
             s = s.Replace(" ", "");
-            IEnumerable<char> ops = from op in _delimeters where op.Value == priority select op.Key;
-            string[] variables = s.Split(_delimeters.Keys.ToArray());
+            // разбить строку на переменные и операторы
+            string[] variables = s.Split(_operators.Keys.ToArray());
             List<char> operators = new List<char>();
             for (int i = 0; i < s.Length; i++) {
-                if (_delimeters.Keys.Contains(s[i])) {
+                if (_operators.Keys.Contains(s[i])) {
                     operators.Add(s[i]);
                 }
             }
-            for(int i = 1; i < operators.Count; i++) {
-                if (ops.Contains(operators[i])) {
+            // определить операторы, которые будем чекать на текущем уровне
+            IEnumerable<char> currentIterationOperators = from op in _operators where op.Value == priority select op.Key;
+            for (int i = 1; i < operators.Count; i++) {
+                if (currentIterationOperators.Contains(operators[i])) {
                     Operation o = new Operation();
                     o.eval1 = variables[i];
                     o.eval2 = variables[i + 1];
+                    if (!(IsNumber(o.eval1) || IsReplacer(o.eval1))) {
+                        _possibleVariables.Add(o.eval1);
+                    }
+                    if (!(IsNumber(o.eval2) || IsReplacer(o.eval2))) {
+                        _possibleVariables.Add(o.eval2);
+                    }
                     o.op = operators[i];
-                    o.outval = replacer.ToString();
+                    string replacer = GenerateReplacer();
+                    o.outval = replacer;
                     queue.Enqueue(o);
                     string expr = $"{variables[i]}{operators[i]}{variables[i + 1]}";
-                    s = s.Replace(expr, replacer.ToString());
-                    replacer++;
+                    s = s.Replace(expr, replacer);
                 }
             }
             if (priority == OperatorPriority.PRIORITY_THIRD) {
